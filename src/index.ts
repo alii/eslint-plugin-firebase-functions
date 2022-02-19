a804ea96ff6e0e87ec7fa581cb6d8ea4d003b677
Alistair Smith <hi@alistair.sh>
2022-02-19 17:26:16 +0100

    fix: emit proper cjs build

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import {safeFunctionExports} from './rules/safe-function-exports';
 
-export default {
+const config = {
 	rules: {
 		'safe-function-exports': safeFunctionExports,
 	},
 };
+
+export default config;
+
+// Stupid CJS hack (why can't everything use esm... cries in the corner)
+if (typeof module !== 'undefined') {
+	module.exports = config;
+	module.exports.default = config;
+}
